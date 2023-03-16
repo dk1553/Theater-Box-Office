9ee96c495f1874da72840b80f879f07b6d0a72d5
dk1553 <daria-line@mail.ru>
2023-03-16 12:05:44 +0100

    removed all files for JPA-test (problem by merging)

--- 3-cleanproject-domain/3-cleanproject-domain/src/main/java/businessObjects/Performance.java
+++ 3-cleanproject-domain/3-cleanproject-domain/src/main/java/businessObjects/Performance.java
@@ -1,26 +1,16 @@
 package businessObjects;
-import  javax.persistence.*;
-@Entity
-@Table(name = "performances")
+
+
 public final class Performance {
-    @Id
-    @Column(name = "name")
+
     private  String name;
-    @Column(name = "description")
+
     private  String description;
 
     public Performance(String name, String description) {
         this.description=description;
         this.name=name;
     }
-
-    protected Performance() {
-
-
-    }
-
-
-
     public String getName() {
         return name;
     }
