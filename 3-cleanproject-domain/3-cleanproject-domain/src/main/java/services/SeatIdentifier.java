1cd7db7a00e5d21c56d29770ea0cd8a35a80f8b5
dk1553 <daria-line@mail.ru>
2023-05-25 15:28:41 +0200

    code cleanUp

--- 3-cleanproject-domain/3-cleanproject-domain/src/main/java/services/SeatIdentifier.java
+++ 3-cleanproject-domain/3-cleanproject-domain/src/main/java/services/SeatIdentifier.java
@@ -4,6 +4,9 @@ public class SeatIdentifier {
     //kein Domain Service, nur ein Hilfsservice
 
     private static int seatID = 0;
+    private SeatIdentifier(){
+        throw new IllegalStateException("Utility class");
+    }
 
     public static int getNewSeatID() {
         seatID++;
